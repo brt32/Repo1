da8f37a6d3d3820bf69c847dc01388b94352fda0
Bartek <bartlomiej.proczkowski@gmail.com>
2021-04-07 16:36:12 +0200

    All Repos 07-04-21

---
+++ Django/myshop-djpta2/manage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Nie można zaimportować Django . Czy jesteś pewien, że jest zainstalowany i "
+            "dostępny w zmiennej środowiskowej PYTHONPATH ? Czy nie "
+            "zapomniałeś aktywować środowiska wirtualnego ?"
+        ) from exc
+    execute_from_command_line(sys.argv)
